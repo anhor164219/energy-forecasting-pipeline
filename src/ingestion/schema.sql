952e0000244cee6f3db1da61bffdb67c496acfcb
anhor164219 <andreas.horst@hotmail.com>
2026-07-13 15:16:07 +0200

    updated the SQL table schema for energy_prices

--- src/ingestion/schema.sql
+++ src/ingestion/schema.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS energy_prices;
 
 CREATE TABLE energy_prices (
     id SERIAL PRIMARY KEY,
-    hour_dk TIMESTAMP NOT NULL,
+    time_dk TIMESTAMP NOT NULL,
     price_area VARCHAR(3) NOT NULL,
     day_ahead_price FLOAT NOT NULL,
     
     -- This ensures we only have ONE price per area per hour
-    CONSTRAINT unique_price_per_area UNIQUE (hour_dk, price_area)
+    CONSTRAINT unique_price_per_area UNIQUE (time_dk, price_area)
 );
